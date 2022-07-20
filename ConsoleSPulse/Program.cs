b7cc45eac447f17fc53efb79889821d53c101b18
Florian Isopp <florian.johannes.isopp@gmail.com>
2022-07-20 12:46:13 +0200

    tearing measurements

--- ConsoleSPulse/Program.cs
+++ ConsoleSPulse/Program.cs
@@ -1,13 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleSPulse;
 using ProxiLABLib;
+using System.Diagnostics;
 
-Console.WriteLine("init keolabs!");
+Debug.WriteLine("init keolabs!");
 
-//var keo = new KeoService();
+var keo = new KeoService();
 
 var proxilab = (IProxiLAB)Activator.CreateInstance(Type.GetTypeFromProgID("KEOLABS.ProxiLAB"));
-var x = KeoService.InitProtocol(proxilab);
+//var x = keo.InitProtocol(proxilab);
 
 //int cycles 500;
 
@@ -15,9 +16,13 @@ byte[] cmd = { 0x00, 0xA4, 0x04, 0x00, 0x08, 0xA0, 0x00, 0x00, 0x01, 0x51, 0x00,
 
 long time = 0;
 
-for (uint i = 500; i < 50000; i += 100)
+for (uint i = 500; i < 510; i += 1)
 {
-    KeoService.SendTclTearing(proxilab, cmd, i, out time);
+    Debug.WriteLine("i:" + i);
+    keo.InitProtocol(proxilab);
+    keo.SendTclTearing(proxilab, cmd, i, out time);
+
+    proxilab.Delay(500);
 }
 
-Console.WriteLine("end");
+Debug.WriteLine("end");
